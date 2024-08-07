5e8254997689b8b46efe77e1e3ef331e98ba883d
kumachandeath <namgungha1@gmail.com>
2024-08-07 16:45:50 +0900

    [level 0] Title: 배열의 평균값, Time: 0.81 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.cs
@@ -1,15 +1,10 @@
 using System;
+using System.Linq;
 
 public class Solution
 {
     public double solution(int[] numbers)
     {
-        double answer = 0;
-        for (int i = 0; i < numbers.Length; i++)
-        {
-            answer += numbers[i];
-        }
-        answer = answer / numbers.Length;
-        return answer;
+        return numbers.Average();
     }
 }
